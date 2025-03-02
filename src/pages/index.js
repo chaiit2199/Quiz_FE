90728b7ff2a32d06cb2f906f617dd4da8b372c42
chaiit2199 <chaiit2199@gmail.com>
2025-03-02 12:50:45 +0700

    update file index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   const { data: session, status } = useSession();
+
   if (status === "loading") {
     return;
   }
 
   if (!session) {
-
     return (
       <div style={{ padding: "20px" }}>
         <h1 className="mb-2">Chào mừng bạn!</h1>
@@ -20,16 +20,12 @@ export default function Home() {
     );
   }
 
-  { console.log(session) }
-
-
   return (
     <div style={{ padding: "20px" }}>
 
       <h1>Xin chào, {session.user?.name || session.user?.email}!</h1>
       <button className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={() => signOut()}>Đăng xuất</button>
-      <h2 className="1231231">{session.user?.role}</h2>
-
+      {session?.user?.role}
     </div>
   );
 }
